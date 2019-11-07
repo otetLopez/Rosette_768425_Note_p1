c21a9716e74c664b04773eee83755618b1acb31f
otetLopez <otet.tud@gmail.com>
2019-11-07 14:31:34 -0500

    Midterm Exam Note : Initial commit and adding of alert when adding a folder

---
+++ Rosette_768425_Note_p1/src/ErrorCodes.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorCodes.swift
+//  Rosette_768425_Note_p1
+//
+//  Created by otet_tud on 11/7/19.
+//  Copyright © 2019 otet_tud. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorCodes {
+    case SUCCESSFUL
+}
