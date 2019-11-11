8a6ddb88927dcfbd6b2c797ffb3f664657bcb9ba
otetLopez <otet.tud@gmail.com>
2019-11-11 15:03:58 -0500

    Exam Note: Changing Edit Button from Bar button Item to navigation item

--- Rosette_768425_Note_p1/CategoryTableViewController.swift
+++ Rosette_768425_Note_p1/CategoryTableViewController.swift
@@ -16,12 +16,12 @@ class CategoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        editButtonLabel.title = "Edit"
+        //editButtonLabel.title = "Edit"
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        //self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
