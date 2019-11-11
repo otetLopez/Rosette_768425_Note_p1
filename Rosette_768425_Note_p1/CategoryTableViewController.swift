2d9ee84da6b65f2f716069ce0ce77be6e870550b
otetLopez <otet.tud@gmail.com>
2019-11-11 14:58:17 -0500

    ******* START OF PART 2 *******

--- Rosette_768425_Note_p1/CategoryTableViewController.swift
+++ Rosette_768425_Note_p1/CategoryTableViewController.swift
@@ -21,7 +21,7 @@ class CategoryTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
